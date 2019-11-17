d6e42e9d700087a638808060a6e4c8c5aaba4c19
Elena <freaking.nothing@gmail.com>
2019-11-18 00:28:01 +0400

    init

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Servy.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :servy,
+      description: "A humble HTTP server",
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Servy, []},
+      env: [port: 4000]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:poison, "~> 4.0"}
+    ]
+  end
+end
